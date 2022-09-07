8d146256c9bd44f3582919998ede9dfe141e95df
hemantjuyal <hemantjuyal@gmail.com>
2022-09-07 12:06:25 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  publicRuntimeConfig:{
+    REACT_APP_PROJECT_ID:'',
+    REACT_APP_API_KEY_SECRET:'',
+    REACT_APP_IPFS_API_ENDPOINT:'',
+    REACT_APP_DEDICATED_GATEWAY:'',
+    NETWORK_ENDPOINT_URL:'',
+  }
+}
