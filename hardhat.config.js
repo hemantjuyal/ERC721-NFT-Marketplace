69ec4669a94b6b54886f95914b9944fce693ea35
hemantjuyal <hemantjuyal@gmail.com>
2022-09-07 12:04:26 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    goreli: {
+      url: process.env.NETWORK_ENDPOINT_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
